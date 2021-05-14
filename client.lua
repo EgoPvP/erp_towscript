2ba87b3b0ddd849631079a18cc90941c18117a2e
ZockerBoys HD <ZockerBoysHD@outlook.de>
2021-05-14 17:44:38 +0200

    copyright notices

--- client.lua
+++ client.lua
@@ -1,3 +1,16 @@
+-- Made by ! 1« NoobKeks_TV »#1337 --
+-- Support & more of my Work - https://discord.gg/6jsHUVMh8G --
+
+-- Copyright (C) 2021 - EgoPvP.com
+
+--This program Is free software: you can redistribute it And/Or modify it under the terms Of the GNU General Public License As published 
+--by the Free Software Foundation, either version 3 Of the License, Or (at your option) any later version.
+
+--This program Is distributed In the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty Of MERCHANTABILITY Or 
+--FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License For more details.
+
+--You should have received a copy Of the GNU General Public License along with this program. If Not, see http://www.gnu.org/licenses/.
+
 ESX = nil 
 
 local isAllowedToTow = true 
@@ -10,5 +23,5 @@ end
 
 if Config.JobRestriction then
     
-    
+
 end
--- config.lua
+++ config.lua
@@ -1,3 +1,17 @@
+-- Made by ! 1« NoobKeks_TV »#1337 --
+-- Support & more of my Work - https://discord.gg/6jsHUVMh8G --
+
+-- Copyright (C) 2021 - EgoPvP.com
+
+--This program Is free software: you can redistribute it And/Or modify it under the terms Of the GNU General Public License As published 
+--by the Free Software Foundation, either version 3 Of the License, Or (at your option) any later version.
+
+--This program Is distributed In the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty Of MERCHANTABILITY Or 
+--FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License For more details.
+
+--You should have received a copy Of the GNU General Public License along with this program. If Not, see http://www.gnu.org/licenses/.
+
+
 Config = {}
 
 Config.Locale = 'de' -- Change to your preferred Language (de, en)
