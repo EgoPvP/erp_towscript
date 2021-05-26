b0468464d49ebf9db48590c7d68afde8ff55b483
ZockerBoys HD <ZockerBoysHD@outlook.de>
2021-05-26 08:09:06 +0200

    fixxed locales
    added en back in there.... Thanks to whoever removed it without me noticing...

---
+++ locales/en.lua
@@ -0,0 +1,13 @@
+Locales['en'] = {
+    ['vehicle_attached'] = '~g~Vehicle towed',
+    ['vehicle_detached'] = '~r~Vehicle unloaded',
+    ['engine_not_stopped'] = '~o~The engine of the vehicle is not switched off!',
+    ['vehicle_not_empty'] = '~o~Someone is still sitting in the vehicle!',
+    ['vehicle_flipped'] = '~r~The vehicle cannot be charged towed down!',
+    ['cant_tow_yourself'] = '~r~I cant tow myself!',
+    ['no_vehicle_found'] = '~r~No vehicle found nearby!',
+    ['vehicle_to_far_away'] = '~r~The vehicle is too far away from the flatbed!',
+    ['not_a_towtruck'] = '~r~My vehicle is not registered as a flatbed!',
+    ['vehicle_blacklisted'] = '~r~I am not allowed to tow this kind of vehicle!',
+    ['no_permissions'] = '~r~You are not allowed to tow vehicles!'
+  }
