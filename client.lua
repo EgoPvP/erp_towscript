743beec08aa43d3a37152d2e8abdfa1c71c5ba33
Lars H <30929430+ZockerBoysHD@users.noreply.github.com>
2021-12-14 17:24:17 +0100

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,5 +1,5 @@
 -- Made by ! 1« NoobKeks_TV »#1337 --
--- Support & more of my Work - https://discord.gg/6jsHUVMh8G --
+-- Support & more of my Work - https://discord.gg/qRc5Hbb --
 -- Copyright (C) 2021 - EgoPvP.com
 -- This program Is free software: you can redistribute it And/Or modify it under the terms Of the GNU General Public License As published 
 -- by the Free Software Foundation, either version 3 Of the License, Or (at your option) any later version.
